c2065c0a3f1dd40b7de3d539c5858b0b4db3905a
iwebnal <iwebnal@gmail.com>
2025-09-17 18:21:58 +0000

    first server commit

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from db.models import Base
+from shared.models.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
